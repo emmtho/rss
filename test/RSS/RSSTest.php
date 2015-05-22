4c4acaa55b1ffcd9e70b3a46de0d86feb6f5b3d2
Emma Thorén <jag_lyser@hotmail.com>
2015-05-22 12:48:21 +0200

    Add

--- test/RSS/RSSTest.php
+++ test/RSS/RSSTest.php
@@ -6,23 +6,20 @@ require_once(__DIR__ . '/../../src/RSS/RSS.php');
 class RSSTester extends \PHPUnit_Framework_TestCase
 {
 
-  public function testConnectToClass()
-  {
-    $this->class = new \Anax\RSS\RSS();
-  }
-
   public function testRSS()
   {
+    $this->class = new \Anax\RSS\RSS();
+
     $xml = "http://dbwebb.se/forum/feed.php";
     $xmlDoc = new \DOMDocument();
     $xmlDoc->load($xml);
 
-    //$test1 = $this->class->setupAndGetTitle($xmlDoc);
-    //$test2 = "Databaser och Webbprogrammering";
-    $this->assertEquals("Databaser och Webbprogrammering", $this->class->setupAndGetTitle($xmlDoc));
+    $test1 = $this->class->setupAndGetTitle($xmlDoc);
+    $this->assertEquals('Databaser och Webbprogrammering', $test1);
 
-    //$this->class->setupAndGetTitle($xmlDoc);
-    //$this->class->getContent($xmlDoc);
+    $test2 = $this->class->getContent($xmlDoc);
+    $this->assertEquals(null ,$test2);
+
   }
 
 }
